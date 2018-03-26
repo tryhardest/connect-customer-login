c3d8e5a700993172eab8c7c03bd50fbdef84f751
Nil Gradisnik <nil.gradisnik@gmail.com>
2018-03-26 16:27:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Layer Connect fake customer login
+
+This is a simple server that enables Layer Connect customer login by generating a JWE token and redirecting to customer console.
+
+> Make sure to set correct `CUSTOMER_CONSOLE_URL` env variable for redirect
+
+## Install dependencies
+
+    yarn
+
+## Start the Server
+
+    yarn start
---
+++ server.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const Koa = require("koa");
+const app = new Koa();
+
+const PORT = process.env.PORT || 5000
+const CONSOLE_URL = process.env.CUSTOMER_CONSOLE_URL || "http://customer.localhost:3000"
+
+const HTML = fs
+  .readFileSync("./index.html")
+  .toString()
+  .replace(/{{CUSTOMER_CONSOLE_URL}}/g, CONSOLE_URL);
+
+app.use(ctx => {
+  ctx.body = HTML;
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on localhost:${PORT}`)
+});
