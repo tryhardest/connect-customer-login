d2679cd33e3c20c7f181235856d5bd2977913c8f
Nil Gradisnik <nil.gradisnik@gmail.com>
2018-03-27 10:36:34 -0700

    Add random user generator, console name env

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const Koa = require("koa");
 const app = new Koa();
 
 const PORT = process.env.PORT || 5000
+
+const CONSOLE_NAME = process.env.CUSTOMER_CONSOLE_NAME || "Fake customer login"
 const CONSOLE_URL = process.env.CUSTOMER_CONSOLE_URL || "http://customer.localhost:3000"
 
 const HTML = fs
   .readFileSync("./index.html")
   .toString()
+  .replace(/{{CUSTOMER_CONSOLE_NAME}}/g, CONSOLE_NAME)
   .replace(/{{CUSTOMER_CONSOLE_URL}}/g, CONSOLE_URL);
 
 app.use(ctx => {
